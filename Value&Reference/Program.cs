fd5c1fdd78db626a7a0028680ee020729a9e9cd3
Elxan Islamov <elkhan.islamov@acsc.az>
2023-11-01 16:37:39 +0400

    task

---
+++ Value&Reference/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] numbers = { 15, 2, 6, 3 };
+Array.Resize(ref numbers, numbers.Length +1 );
+int[] numbers2 = { 1, 2, 3, 4, };
+Array.Resize(ref numbers, numbers2.Length );
+foreach (var number in numbers)
+   
+    {
+    Console.WriteLine(number);
+   
+}
+foreach (var number in numbers2)
+{
+   Console.WriteLine(number);
+}
+
+
+
